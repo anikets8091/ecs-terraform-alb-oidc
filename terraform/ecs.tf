9567b3c3a08df34e7e4f44456fe456d8dfe0bd5c
anikets8091 <anikets8091@gmail.com>
2025-08-09 15:48:27 +0530

    up

---
+++ terraform/ecs.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_attach" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_cluster" "app_cluster" {
+  name = "myapp-cluster"
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "myapp-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([{
+    name      = "myapp"
+    image     = "${aws_ecr_repository.app_repo.repository_url}:latest"
+    essential = true
+    portMappings = [{ containerPort = 80, hostPort = 80 }]
+  }])
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "myapp-service"
+  cluster         = aws_ecs_cluster.app_cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    assign_public_ip = true
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "myapp"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
