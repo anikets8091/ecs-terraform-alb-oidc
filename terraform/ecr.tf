19cbe6e83256ee9efe43565118a5516d58f75088
anikets8091 <anikets8091@gmail.com>
2025-08-09 17:59:16 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "app_repo" {
-  name                 = "myapp-repo"
+  name                 = "myapp-repo-1"
   image_scanning_configuration { scan_on_push = true }
 }
 
