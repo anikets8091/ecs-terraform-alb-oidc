2c197d95264b83a7167f7ec7ca4518bf2124a9c8
anikets8091 <anikets8091@gmail.com>
2025-08-09 06:50:52 +0530

    new script

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,5 @@
+output "alb_dns" {
+  description = "Public DNS name of the ALB"
+  value       = aws_lb.app_alb.dns_name
+}
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,6 @@
+variable "region" {
+  description = "this is region name"
+  type    = string
+  default = "us-east-1"
+}
 
